46635669b981cb81b0ba38651c00a4bc9cfbeaf2
Bar <bar@makesmithtech.com>
2015-04-25 12:42:16 -0700

    page transitions added

--- cncgcKivy.py
+++ cncgcKivy.py
@@ -6,12 +6,13 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.graphics import Color, Ellipse
 from kivy.graphics import InstructionGroup
 from kivy.core.window import Window
+from kivy.uix.button import Button
 from kivy.lang import Builder
 from kivy.clock import Clock
-from math import cos, sin
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 
 
-class FrontPage(FloatLayout):
+class FrontPage(Screen):
     exploreplot = ObjectProperty(None)
     exampleplot = ObjectProperty(None)
     
@@ -21,11 +22,54 @@ class FrontPage(FloatLayout):
     def testFuncTwo(self):
         print "func two"
 
+class OtherFeatures(Screen):
+    pass
+
+class SoftwareSettings(Screen):
+    pass
+
 class GroundControlApp(App):
     def build(self):
-        interface =  FrontPage()
+        interface = FloatLayout()
+        
+        screenControls = GridLayout(rows = 1)
+        btn1 = Button(text='Front Page', size_hint=(None, None), width = 320, height = 30)
+        btn1.bind(on_press=self.showFront)
+        screenControls.add_widget(btn1)
+        
+        btn2 = Button(text='Other Features', size_hint=(None, None), width = 320, height = 30)
+        btn2.bind(on_press=self.showFeatures)
+        screenControls.add_widget(btn2)
+        
+        
+        btn3 = Button(text='Settings', size_hint=(None, None), width = 320, height = 30)
+        btn3.bind(on_press=self.showSettings)
+        screenControls.add_widget(btn3)
+        
+        interface.add_widget(screenControls)
+        
+        
+        self.sm = ScreenManager(transition=SlideTransition(), size_hint=(None, None), pos = (120,0), width = Window.width-120, height = Window.height, clearcolor=(1,1,1,1))
+        
+        self.frontpage = FrontPage(name='FrontPage')
+        self.sm.add_widget(self.frontpage)
+        
+        self.otherfeatures = OtherFeatures(name='OtherFeatures')
+        self.sm.add_widget(self.otherfeatures)
+        
+        self.softwaresettings = SoftwareSettings(name='SoftwareSettings')
+        self.sm.add_widget(self.softwaresettings)
+        
+        interface.add_widget(self.sm)
+        
         return interface
 
-
+    def showFront(self, extra):
+        self.sm.current = 'FrontPage'
+    def showFeatures(self, extra):
+        self.sm.current = 'OtherFeatures'
+    def showSettings(self, extra):
+        self.sm.current = 'SoftwareSettings'
+    
 if __name__ == '__main__':
     GroundControlApp().run()
