e6d62c30bd1587db56669aef0a055a42431490ee
Bar <bar@makesmithtech.com>
2015-05-17 13:02:09 -0700

    added popup dialog templates

--- cncgcKivy.py
+++ cncgcKivy.py
@@ -418,6 +418,34 @@ class ManualControl(FloatLayout):
 
 '''
 
+Pop-Up Classes
+
+Classes to define the templates for pop-ups of different types.
+
+'''
+
+class LoadDialog(FloatLayout):
+    '''
+    
+    A Pop-up Dialog To Open A File
+    
+    '''
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+class SaveDialog(FloatLayout):
+    '''
+    
+    A Pop-up Dialog To Save A File
+    
+    '''
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+
+
+'''
+
 Data Classes
 
 '''
