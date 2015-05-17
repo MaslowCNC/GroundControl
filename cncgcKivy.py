33eaa7fdbc8b8f31fdd15e412cca205c50320636
Bar <bar@makesmithtech.com>
2015-05-17 15:02:40 -0700

    gcode file opens and passes parsed gcode to canvas

--- cncgcKivy.py
+++ cncgcKivy.py
@@ -15,6 +15,7 @@ from kivy.uix.button import Button
 from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.uix.popup import Popup
 
 '''
 
@@ -27,6 +28,7 @@ import Queue
 import serial
 from time import time
 import sys
+import re
 
 '''
 
@@ -35,8 +37,8 @@ UI Elements
 '''
 
 class GcodeCanvas(FloatLayout):
-    crossPosX = NumericProperty(25)
-    crossPosY = NumericProperty(7)
+    crossPosX = NumericProperty(0)
+    crossPosY = NumericProperty(0)
     
     offsetX = NumericProperty(300)
     offsetY = NumericProperty(200)
@@ -45,17 +47,21 @@ class GcodeCanvas(FloatLayout):
     
     canvasScaleFactor = 2 #scale from mm to pixels
     
+    gcode = []
+    
+    def updateGcode(self):
+        print "update gcode"
+        print self.gcode
     
     def setCrossPos(self, xPos, yPos):
         self.crossPosX = xPos * self.canvasScaleFactor
         self.crossPosY = yPos * self.canvasScaleFactor
     
-    
     def onMotion(self, etype, callback ,motionevent):
         if motionevent.x != 0.0:
-            if abs(motionevent.x - self.lastTouchX) > 100:
+            if abs(motionevent.x - self.lastTouchX) > 50:
                 self.lastTouchX = motionevent.x
-            if abs(motionevent.y - self.lastTouchY) > 100:
+            if abs(motionevent.y - self.lastTouchY) > 50:
                 self.lastTouchY = motionevent.y
             
             self.offsetX =  self.offsetX + (motionevent.x - self.lastTouchX)
@@ -209,13 +215,89 @@ class FrontPage(Screen):
             self.spindleFlag = 1
 
 class OtherFeatures(Screen):
-    pass
+    viewmenu = ObjectProperty(None) #make viewmenu object accessable at this scope
 
 class SoftwareSettings(Screen):
     pass
 
 class ViewMenu(GridLayout):
-    pass
+    gcodeFilePath = ""
+    
+    def openFile(self):
+        '''
+        
+        Open A File
+        
+        '''
+        self.show_load()
+    
+    def setGcodeLocation(self,gcodeLocation):
+        self.gcodeCanvas = gcodeLocation
+        
+        #self.gcodeCanvas.gcode = ["G01 X10", "G02 X Y I J"]
+        #gcodeLocation.updateGcode()
+    
+    def show_load(self):
+        '''
+        
+        Open The Pop-up To Load A File
+        
+        Creates a new pop-up which can be used to open a file.
+        
+        '''
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+    
+    def load(self, path, filename):
+        '''
+        from kivy.uix.popup import Popup
+        Load A File (Any Type)
+        
+        Takes in a file path (from pop-up) and handles the file appropriately for the given file-type.
+        
+        '''
+        #locate the file extension
+        filename = filename[0]
+        fileExtension = filename[-4:]
+        
+        self.gcodeFilePath = filename
+        print self.gcodeFilePath
+        self.reloadGcode()
+        self.dismiss_popup()
+    
+    def reloadGcode(self):
+        #This reloads the gcode from the hard drive in case it has been updated.
+        try:
+            filename = self.gcodeFilePath
+            filterfile = open(filename, 'r')
+            rawfilters = filterfile.read()
+            filtersparsed = re.split(r'\s(?=G)|\n|\s(?=g)|\s(?=M)', rawfilters) #splits the gcode into elements to be added to the list
+            filtersparsed = [x + ' ' for x in filtersparsed] #adds a space to the end of each line
+            filtersparsed = [x.lstrip() for x in filtersparsed]
+            filtersparsed = [x.replace('X ','X') for x in filtersparsed]
+            filtersparsed = [x.replace('Y ','Y') for x in filtersparsed]
+            filtersparsed = [x.replace('Z ','Z') for x in filtersparsed]
+            filtersparsed = [x.replace('I ','I') for x in filtersparsed]
+            filtersparsed = [x.replace('J ','J') for x in filtersparsed]
+            filtersparsed = [x.replace('F ','F') for x in filtersparsed]
+
+            self.gcodeCanvas.gcode = filtersparsed
+            filterfile.close() #closes the filter save file
+        except:
+            if filename is not "":
+                print "Cannot reopen gcode file. It may have been moved or deleted. To locate it or open a different file use File > Open G-code"
+            self.gcodeFilePath = ""
+        self.gcodeCanvas.updateGcode()
+    
+    def dismiss_popup(self):
+        '''
+        
+        Close The Pop-up
+        
+        '''
+        self._popup.dismiss()
 
 class RunMenu(FloatLayout):
     pass
@@ -566,6 +648,8 @@ class GroundControlApp(App):
         
         self.otherfeatures.connectmenu.setupQueues(message_queue, gcode_queue, quick_queue)
         self.frontpage.setupQueues(message_queue, gcode_queue, quick_queue)
+        
+        self.otherfeatures.viewmenu.setGcodeLocation(self.frontpage.gcodecanvas)
         
         Clock.schedule_interval(self.otherfeatures.connectmenu.detectCOMports, 2)
         Clock.schedule_interval(self.runPeriodically, .1)
