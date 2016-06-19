da5f2c6f59ef7000127ddbdbfe23992f019c60dc
Bar Smith <bar@makesmithtech.com>
2016-06-18 19:13:12 -0700

    save point

---
+++ UIElements/ScrollableStencilLabel.py
@@ -0,0 +1,15 @@
+from kivy.uix.floatlayout    import    FloatLayout
+from kivy.properties         import    ObjectProperty
+from kivy.properties         import    StringProperty
+
+
+class ScrollableStencilLabel():
+    '''
+    
+    A Pop-up Dialog To Display Text
+    
+    If the text does not all fit on the display, the user can scroll it
+    
+    '''
+    cancel = ObjectProperty(None)
+    text = StringProperty("")
