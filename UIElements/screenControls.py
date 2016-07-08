e89ff77eb97e8bc0c82667ff309de31da51d19c6
Bar Smith <bar@makesmithtech.com>
2016-07-07 21:59:33 -0700

    settings added

--- UIElements/screenControls.py
+++ UIElements/screenControls.py
@@ -1,18 +1,33 @@
 from kivy.uix.gridlayout               import   GridLayout
 from kivy.uix.popup                    import   Popup
 from UIElements.otherFeatures          import   OtherFeatures
+from UIElements.softwareSettings       import   SoftwareSettings
 
 class ScreenControls(GridLayout):
     
     def show_actions(self):
         '''
         
-        Open The Pop-up To Allow User Actions
+        Open A Pop-up To Allow User Actions
         
         Creates a new pop-up allows the user to do things like open a file.
         
         '''
         content = OtherFeatures()
+        self._popup = Popup(title="Actions", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+        
+    
+    def show_settings(self):
+        '''
+        
+        Open A Settings Popup
+        
+        Creates a new pop-up which lets the user see and edit the program settings
+        
+        '''
+        content = SoftwareSettings()
         self._popup = Popup(title="Actions", content=content,
                             size_hint=(0.9, 0.9))
         self._popup.open()
