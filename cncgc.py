caac109898a94ee4d5da673ae64b92ea1a4ff989
Bar Smith <bar@makesmithtech.com>
2016-08-09 16:53:52 -0700

    cleanup

--- cncgc.py
+++ cncgc.py
@@ -10,7 +10,6 @@ from kivy.uix.anchorlayout      import AnchorLayout
 from kivy.core.window           import Window
 from kivy.uix.button            import Button
 from kivy.clock                 import Clock
-from kivy.config                import Config
 
 
 '''
