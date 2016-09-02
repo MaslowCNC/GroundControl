8db9fc32f35f3b3fe5aaa81afa20b2e977d4cdc0
Bar Smith <bar@makesmithtech.com>
2016-09-02 11:51:53 -0700

    angle in

--- Simulation/simulationSled.py
+++ Simulation/simulationSled.py
@@ -15,8 +15,11 @@ class SimulationSled(FloatLayout):
     lineOne = None
     lineTwo = None
     
-    sledPointOne = ObjectProperty([0,0]) #top right corner
-    sledPointTwo = ObjectProperty([0,0]) #top left corner
+    sledPointOne    = ObjectProperty([0,0]) #top right corner
+    sledPointTwo    = ObjectProperty([0,0]) #top left corner
+    sledToolPos     = ObjectProperty([0,0]) #top left corner
+    sledMidpointTop = ObjectProperty([0,0])
+    
     sledHeight = 100
     sledWidth  = 200
     
@@ -30,7 +33,9 @@ class SimulationSled(FloatLayout):
         self.end = end
         
         self.lineOne.bind(fromPos = self.updateSled)
-        self.lineOne.bind(toPos = self.updateSled)
+        self.lineOne.bind(toPos   = self.updateSled)
+        self.lineTwo.bind(fromPos = self.updateSled)
+        self.lineTwo.bind(toPos   = self.updateSled)
         
         self.updateSled()
     
@@ -41,9 +46,6 @@ class SimulationSled(FloatLayout):
         x = distUpLine/math.sqrt(math.pow(self.lineTwo.slope,2) + 1)
         y = self.lineTwo.slope*x
         
-        print "this"
-        print y
-        
         xDist = self.lineTwo.toPos[0] + x
         yDist = self.lineTwo.toPos[1] + y
         
@@ -52,8 +54,6 @@ class SimulationSled(FloatLayout):
         
         x = distUpLine/math.sqrt(math.pow(self.lineOne.slope,2) + 1)
         y = self.lineOne.slope*x
-        
-        print y
         
         xDist = self.lineTwo.toPos[0] - x
         yDist = self.lineTwo.toPos[1] - y
@@ -70,6 +70,23 @@ class SimulationSled(FloatLayout):
         
         self.slant = math.degrees(math.atan(slopeBetweenPoints))
         self.slantAsString = "Slant: " + str(self.slant)
+        
+        self.sledMidpointTop[0] = (self.sledPointOne[0]+self.sledPointTwo[0])/2
+        self.sledMidpointTop[1] = (self.sledPointOne[1]+self.sledPointTwo[1])/2
+        
+        
+        perpindicularSlope = -10000
+        try:
+            perpindicularSlope = -1/slopeBetweenPoints
+        
+        except:
+            pass
+            
+        x = self.sledHeight/math.sqrt(math.pow(perpindicularSlope,2) + 1)
+        y = perpindicularSlope*x
+        
+        self.sledToolPos[0] = self.sledMidpointTop[0] + x
+        self.sledToolPos[1] = self.sledMidpointTop[1] + y
         
         
     
