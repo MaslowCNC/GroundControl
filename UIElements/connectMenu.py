bb79c15837ea2f1b2e5d878910385cb3e0647276
Scott Smith <scottsm@mac.com>
2017-03-20 21:46:32 -0700

    Patch only the darwin platform
    Win and linux work properly with previous approach, patch only the case
    where platform is darwin

--- UIElements/connectMenu.py
+++ UIElements/connectMenu.py
@@ -27,16 +27,18 @@ class ConnectMenu(FloatLayout, MakesmithInitFuncs):
         portsList = ["Available Ports:"]
         
         if sys.platform.startswith('win'):
-        	sysports = ['COM%s' % (i + 1) for i in range(256)]
+            for ports in self.data.serialPort.listSerialPorts():
+                portsList.append(port)
         elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
             # this excludes your current terminal "/dev/tty"
-        	sysports = glob.glob('/dev/tty[A-Za-z]*')
+            for ports in self.data.serialPort.listSerialPorts():
+                portsList.append(port)
         elif sys.platform.startswith('darwin'):
-        	sysports = glob.glob('/dev/tty.*')
+            sysports = glob.glob('/dev/tty.*')
+            for port in sysports:
+                portsList.append(port)
         else:
-        	raise EnvironmentError('Unsupported platform')
-        for port in sysports:
-        	portsList.append(port)
+            raise EnvironmentError('Unsupported platform')
         
         if len(portsList) == 1:
             portsList.append("None")
