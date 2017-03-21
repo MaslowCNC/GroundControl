12c7fa9912bef8ce8dcb5694347ce333b88c228e
Scott Smith <scottsm@mac.com>
2017-03-21 12:31:54 -0700

    Revert "Patch otherFeatures.py to correct serial issue"
    This reverts commit 2860bbe2a14b378842f12a4c4ae5522793cfb923.

--- UIElements/connectMenu.py
+++ UIElements/connectMenu.py
@@ -3,7 +3,6 @@ from kivy.properties                           import  ListProperty
 from DataStructures.makesmithInitFuncs         import  MakesmithInitFuncs
 
 import sys
-import glob
 import serial
 import threading
 
@@ -21,22 +20,12 @@ class ConnectMenu(FloatLayout, MakesmithInitFuncs):
         self.data.config.set('Maslow Settings', 'COMport', str(self.data.comport))
         self.data.config.write()
         
-    
     def updatePorts(self, *args):
         
         portsList = ["Available Ports:"]
         
-        if sys.platform.startswith('win'):
-        	sysports = ['COM%s' % (i + 1) for i in range(256)]
-        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-            # this excludes your current terminal "/dev/tty"
-        	sysports = glob.glob('/dev/tty[A-Za-z]*')
-        elif sys.platform.startswith('darwin'):
-        	sysports = glob.glob('/dev/tty.*')
-        else:
-        	raise EnvironmentError('Unsupported platform')
-        for port in sysports:
-        	portsList.append(port)
+        for port in self.data.serialPort.listSerialPorts():
+            portsList.append(port)
         
         if len(portsList) == 1:
             portsList.append("None")
