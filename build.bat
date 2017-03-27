a329e10b2e45b0c91aac2f7d6a00ebc8714042df
Barbour Smith <bar@makesmithtech.com>
2017-03-27 13:41:50 -0700

    update build.bat to delete un-needed files

--- build.bat
+++ build.bat
@@ -1,3 +1,10 @@
 python -m PyInstaller --clean --win-private-assemblies --onefile GroundControl.spec
 rmdir build /s /Q
 rmdir .\dist\GroundControl\gcodeForTesting /s /Q
+rmdir .\dist\GroundControl\.git /s /Q
+rmdir .\dist\GroundControl\build /s /Q
+rmdir .\dist\GroundControl\Connection /s /Q
+rmdir .\dist\GroundControl\DataStructures /s /Q
+rmdir .\dist\GroundControl\Documentation /s /Q
+rmdir .\dist\GroundControl\Simulation /s /Q
+rmdir .\dist\GroundControl\UIElements /s /Q
