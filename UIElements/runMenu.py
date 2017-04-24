e93795d897c762daf40fa211a03e745a08709f12
Scott Smith <scottsm@mac.com>
2017-04-24 16:04:02 -0700

    Fix-z-axis-return-to-center
    This reverts commit 8349ad02314c7b56868ec231a010fd297086908d, reversing
    changes made to e2de0eab035a90a85c1e7b00c78e53c973422115.

--- UIElements/runMenu.py
+++ UIElements/runMenu.py
@@ -12,6 +12,20 @@ class RunMenu(FloatLayout):
         App.get_running_app().stop()
     
     def returnToCenter(self):
-        self.data.gcode_queue.put("G00 Z0 ")
-        self.data.gcode_queue.put("G00 X0 Y0 Z0 ")
-        self.parentWidget.close()
+        # self.data.gcode_queue.put("G00 Z0.25 ")
+        # self.data.gcode_queue.put("G00 X0 Y0 ")
+        # self.data.gcode_queue.put("G00 Z0 ")
+        # self.parentWidget.close()
+#if the machine has a z-axis lift it then go home
+        if int(self.data.config.get('Maslow Settings', 'zAxis')):
+            if self.data.units == "INCHES":
+                self.data.gcode_queue.put("G00 Z.25 ")
+            else:
+                self.data.gcode_queue.put("G00 Z5.0 ")
+            
+            self.data.gcode_queue.put("G01 X" + str(self.data.gcodeShift[0]) + " Y" + str(self.data.gcodeShift[1]) + " ")
+            
+            self.data.gcode_queue.put("G00 Z0 ")
+        #if the machine does not have a z-axis, just go home
+        else:
+            self.data.gcode_queue.put("G00 X" + str(self.data.gcodeShift[0]) + " Y" + str(self.data.gcodeShift[1]) + " ")
