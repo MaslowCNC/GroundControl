620dd380eae8e39cc461dbc3bab9f88b5cffb82f
Barbour Smith <bar@makesmithtech.com>
2017-05-01 14:10:04 -0700

    advancing the chain works

--- UIElements/measureMachinePopup.py
+++ UIElements/measureMachinePopup.py
@@ -28,4 +28,18 @@ class MeasureMachinePopup(GridLayout):
         print "right CCW"
         self.data.gcode_queue.put("G91 \nB09 R.5 \nG90 ")
     
+    def extendLeft(self, dist):
+        print "Extend left by " + str(dist) + "mm"
+        self.data.gcode_queue.put("G91 \nB09 L" + str(dist) + " \nG90 ")
+    
+    def retractLeft(self, dist):
+        print "Retract left by " + str(dist) + "mm"
+        self.data.gcode_queue.put("G91 \nB09 L-" + str(dist) + " \nG90 ")
+    
+    def setZero(self):
+        #mark that the sprockets are straight up
+        self.data.gcode_queue.put("B06 L0 R0 ");
+    
+    def measureLeft(self):
+        print "would measure"
     
