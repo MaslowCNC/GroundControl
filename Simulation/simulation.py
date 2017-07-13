968c0bffc7738b67336c5fbb4ee807cf15e3c89f
Barbour Smith <bar@makesmithtech.com>
2017-07-13 10:27:03 -0700

    sliders in place

--- Simulation/simulation.py
+++ Simulation/simulation.py
@@ -27,13 +27,8 @@ class SimulationApp(App):
         self.simulationCanvas = SimulationCanvas()
         interface.add_widget(self.simulationCanvas)
         
-        Clock.schedule_once(self.initialize, 2)
-        
-        
         return interface
         
-    def initialize(self, *args):
-        self.simulationCanvas.initialize()
         
         
 if __name__ == '__main__':
--- Simulation/simulationCanvas.py
+++ Simulation/simulationCanvas.py
@@ -1,20 +1,15 @@
-from kivy.uix.floatlayout                    import FloatLayout
+from kivy.uix.gridlayout                     import GridLayout
 from kivy.properties                         import NumericProperty, ObjectProperty
 from kivy.graphics                           import Color, Ellipse, Line
 from kivy.graphics.transformation            import Matrix
 from kivy.core.window                        import Window
-from simulationLine                          import SimulationLine
-from simulationAngle                         import SimulationAngle
-from simulationSled                          import SimulationSled
 from kinematics                              import Kinematics
-from chainLengthToXY                         import ChainLengthtoXY
-from posToChainLength                        import PosToChainLength
 from kivy.graphics.transformation            import Matrix
 
 import re
 import math
 
-class SimulationCanvas(FloatLayout):
+class SimulationCanvas(GridLayout):
     scatterObject     = ObjectProperty(None)
     
     bedWidth           = 2438.4 #8'
@@ -28,56 +23,8 @@ class SimulationCanvas(FloatLayout):
     
     
     def initialize(self):
+        pass
         
-        self.startChains()
-        self.drawFrame()
-        
-        self.setSpindleLocation(self.bedWidth/2,self.bedHeight/2)
-        
-        self.setInitialZoom()
-        
-        self.xPosSlider.bind(value=self.xPosSliderValueChange)
-        self.yPosSlider.bind(value=self.yPosSliderValueChange)
-        
-        self.setupSled()
-        
-        
-    def setSpindleLocation(self,x,y):
-        
-        self.sled.setXY(x,y)
-    
-    def xPosSliderValueChange(self,callback,value):
-        self.setSpindleLocation(value,self.yPosSlider.value)
-        
-    def yPosSliderValueChange(self,callback,value):
-        self.setSpindleLocation(self.xPosSlider.value, value)
-    
-    def drawFrame(self):
-        self.frameLeft.initialize()
-        self.frameTop.initialize()
-        self.frameRight.initialize()
-        self.frameBottom.initialize()
-        
-        
-        self.frameLeft.setStart(0,0)
-        self.frameLeft.setEnd(0,self.bedHeight)
-        self.frameLeft.color = (1,0,0)
-        
-        self.frameTop.setStart(0,self.bedHeight)
-        self.frameTop.setEnd(self.bedWidth,self.bedHeight)
-        self.frameTop.color = (1,0,0)
-        
-        self.frameRight.setStart(self.bedWidth,0)
-        self.frameRight.setEnd(self.bedWidth,self.bedHeight)
-        self.frameRight.color = (1,0,0)
-        
-        self.frameBottom.setStart(0,0)
-        self.frameBottom.setEnd(self.bedWidth,0)
-        self.frameBottom.color = (1,0,0)
-    
-    def setupSled(self):
-        self.sled.initialize(self.chainA, self.chainB, 1)
-    
     def setInitialZoom(self):
         mat = Matrix().scale(.4, .4, 1)
         self.scatterInstance.apply_transform(mat, (0,0))
@@ -85,15 +32,10 @@ class SimulationCanvas(FloatLayout):
         mat = Matrix().translate(200, 100, 0)
         self.scatterInstance.apply_transform(mat)
     
-    def startChains(self):
-        self.chainA.initialize()
-        self.chainB.initialize()
-        self.lineT.initialize()
-        self.lineT.color = (0,0,1)
-        
-        
-        self.chainA.setStart(-self.motorTranslate, self.motorY)
-        self.chainB.setStart(self.motor2X, self.motorY)
-        
-        self.lineT.setStart(-self.motorTranslate,self.motorY)
-        self.lineT.setEnd(self.motor2X,self.motorY)
+    def resetSliders(self):
+        print "connection made"
+        self.motorSpacingError.value = 0
+        self.motorVerticalError.value = 0
+        self.sledMountSpacingError.value = 0
+        self.sledCGError.value = 0
+    
