69dc1fab432a408c434d75cd365f4da4d7a77766
David Lang <david@lang.hm>
2017-07-14 02:29:26 -0700

    add machine details to the output
    show the dimensions of the ideal machine and the 'actual' machine that
    would generate the distortions

--- Simulation/simulationCanvas.py
+++ Simulation/simulationCanvas.py
@@ -223,6 +223,9 @@ class SimulationCanvas(GridLayout):
         self.distortedKinematics.h3 = self.correctKinematics.h3 + self.vertCGDist.value
         self.vertCGDistLabel.text = "Vert Dist\nBit to CG Error: " + str(int(self.vertCGDist.value)) + "mm"
         
+        self.machineLabel.text = "distance between sled attachments ideal: "+str(self.correctKinematics.l)+" actual: "+str(self.distortedKinematics.l)+"mm\nvertical distance between sled attachments and bit ideal: "+str(self.correctKinematics.s)+" actual: "+str(self.distortedKinematics.s)+"mm\nvertical distance between sled attachments and CG ideal: "+str(self.correctKinematics.h3+self.correctKinematics.s)+" actual: "+str(self.distortedKinematics.h3+self.distortedKinematics.s)+"mm\ndistance between motors ideal: "+str(self.correctKinematics.D)+" actual: "+str(self.distortedKinematics.D)+"mm"
+
+
         self.distortedKinematics.recomputeGeometry()
     
     def drawOutline(self):
