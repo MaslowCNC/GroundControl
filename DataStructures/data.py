3a6d9bd98885eb2642859aa2cbad01f81edd04de
Barbour Smith <bar@makesmithtech.com>
2017-09-28 09:43:08 -0700

    bump version number

--- DataStructures/data.py
+++ DataStructures/data.py
@@ -25,7 +25,7 @@ class Data(EventDispatcher):
     
     #Gcodes contains all of the lines of gcode in the opened file
     gcode      = ObjectProperty([])
-    version    = '0.91'
+    version    = '0.92'
     #all of the available COM ports
     comPorts   = []
     #This defines which COM port is used
