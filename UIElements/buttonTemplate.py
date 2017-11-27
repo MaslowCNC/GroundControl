7b48ef4f0982a64559a2d8d41f586ecdf83ac579
Barbour Smith <bar@makesmithtech.com>
2017-11-27 13:34:15 -0800

    buttons are working

--- UIElements/buttonTemplate.py
+++ UIElements/buttonTemplate.py
@@ -1,7 +1,25 @@
-from kivy.properties      import StringProperty
-from kivy.uix.button          import Button
+from kivy.properties          import StringProperty
+from kivy.properties          import ObjectProperty
+from kivy.uix.behaviors       import ButtonBehavior
+from kivy.uix.floatlayout     import FloatLayout
 
-class ButtonTemplate(Button):
-    btnBackground     = StringProperty('atlas://data/images/defaulttheme/button')
-    btnBackgroundDown = StringProperty('atlas://data/images/defaulttheme/button_pressed')
+class ButtonTemplate(FloatLayout, ButtonBehavior):
+    btnBackground        = StringProperty('atlas://data/images/defaulttheme/button')
+    btnBackgroundDown    = StringProperty('atlas://data/images/defaulttheme/button_pressed')
+    funcToCallOnPress    = ObjectProperty(None)
+    funcToCallOnRelease  = ObjectProperty(None)
+    text                 = StringProperty("")
     
+    
+    def internal_on_press(self, *args):
+        print "internal on_press ran" 
+        
+        if self.funcToCallOnPress is not None:
+            self.funcToCallOnPress()
+        
+        
+    def internal_on_release(self, *args):
+        print "internal on_release ran"
+        
+        if self.funcToCallOnRelease is not None:
+            self.funcToCallOnRelease()
