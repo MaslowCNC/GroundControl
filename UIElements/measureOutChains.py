511b4257937e06b8467c31199fa3064b4d1abd68
Barbour Smith <bar@makesmithtech.com>
2017-11-29 12:04:17 -0800

    Add a timed delay before starting to extend the chains

--- UIElements/measureOutChains.py
+++ UIElements/measureOutChains.py
@@ -1,5 +1,6 @@
 from kivy.uix.widget                      import   Widget
 from kivy.properties                      import   ObjectProperty, StringProperty
+from kivy.clock                           import   Clock
 
 class MeasureOutChains(Widget):
     '''
@@ -9,16 +10,54 @@ class MeasureOutChains(Widget):
     '''
     data              =  ObjectProperty(None) #set externally
     text              =  StringProperty("")
+    countDownTime     =  0
     
     def stop(self):
         self.data.quick_queue.put("!") 
         with self.data.gcode_queue.mutex:
             self.data.gcode_queue.queue.clear()
     
-    def calibrateChainLengths(self, direction):
-        cmdString = "B02 " + str(direction) + " "
-        self.data.gcode_queue.put("B02 ")
-    
     def next(self):
         self.data.gcode_queue.put("B15 ")
         self.carousel.load_next()
+    
+    '''
+    Left Chain
+    '''
+    def startCountDownL(self):
+        self.countDownTime  = 5
+        
+        Clock.schedule_once(self.countingDownL, 1)
+        self.leftChainBtn.text = '   Extend\nLeft Chain (' + str(self.countDownTime) + ")"
+    
+    def countingDownL(self, *args):
+        self.countDownTime = self.countDownTime - 1
+        
+        self.leftChainBtn.text = '   Extend\nLeft Chain (' + str(self.countDownTime) + ")"
+        
+        if self.countDownTime > 0:
+            Clock.schedule_once(self.countingDownL, 1)
+        else:
+            self.leftChainBtn.text = '   Extend\nLeft Chain'
+            self.data.gcode_queue.put("B02 L ")
+    
+    '''
+    Right Chain
+    '''
+    
+    def startCountDownR(self):
+        self.countDownTime  = 5
+        
+        Clock.schedule_once(self.countingDownR, 1)
+        self.rightChainBtn.text = '   Extend\nRight Chain (' + str(self.countDownTime) + ")"
+    
+    def countingDownR(self, *args):
+        self.countDownTime = self.countDownTime - 1
+        
+        self.rightChainBtn.text = '   Extend\nRight Chain (' + str(self.countDownTime) + ")"
+        
+        if self.countDownTime > 0:
+            Clock.schedule_once(self.countingDownR, 1)
+        else:
+            self.rightChainBtn.text = '   Extend\nRight Chain'
+            self.data.gcode_queue.put("B02 R ")
