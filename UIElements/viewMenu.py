40d2418cdbbe8a31041f0851ae4fa341687a4324
Barbour Smith <bar@makesmithtech.com>
2017-12-27 14:54:13 -0800

    Fully working?

--- UIElements/viewMenu.py
+++ UIElements/viewMenu.py
@@ -29,18 +29,17 @@ class ViewMenu(GridLayout, MakesmithInitFuncs):
         initialDir = path.dirname(self.data.gcodeFile)
         if initialDir is "": 
             initialDir = path.expanduser('~')
-        filename = askopenfilename( initialdir = initialDir) # show an "Open" dialog box and return the path to the selected file
+        validExtensions = self.data.config.get('Ground Control Settings', 'validExtensions').replace(",", "")
+        filename = askopenfilename( initialdir = initialDir, filetypes = (("Gcode files", validExtensions),("all files","*.*")), title = "Open Gcode File") # show an "Open" dialog box and return the path to the selected file
         
-        self.data.gcodeFile = filename
+        if filename is not None:
+            self.data.gcodeFile = filename
+            self.data.config.set('Maslow Settings', 'openFile', str(self.data.gcodeFile))
+            self.data.config.write()
         
-        #content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
-        #content.path = path.dirname(self.data.gcodeFile)
-        #if content.path is "": 
-        #    content.path = path.expanduser('~')
-        #self._popup = Popup(title="Load file", content=content,
-        #                    size_hint=(0.9, 0.9))
-        #self._popup.open()
-    
+        #close the parent popup
+        self.parentWidget.close()
+        
     def reloadGcode(self):
         '''
         
@@ -51,34 +50,6 @@ class ViewMenu(GridLayout, MakesmithInitFuncs):
         filePath = self.data.gcodeFile
         self.data.gcodeFile = ""
         self.data.gcodeFile = filePath
-        
-        #close the parent popup
-        self.parentWidget.close()
-    
-    def load(self, filePath, filename):
-        '''
-        
-        Load A File (Any Type)
-        
-        Takes in a file path (from pop-up) and handles the file appropriately for the given file-type.
-        
-        '''
-        
-        #close the open file popup
-        self.dismiss_popup()
-        
-        #locate the file
-        filename = filename[0]
-        fileExtension = path.splitext(filename)[1]
-        
-        validExtensions = self.data.config.get('Ground Control Settings', 'validExtensions').replace(" ", "").split(',')
-        
-        if fileExtension in validExtensions:
-            self.data.gcodeFile = filename
-            self.data.config.set('Maslow Settings', 'openFile', str(self.data.gcodeFile))
-            self.data.config.write()
-        else:
-            self.data.message_queue.put("Message: Ground control can only open gcode files with extensions: " + self.data.config.get('Ground Control Settings', 'validExtensions'))
         
         #close the parent popup
         self.parentWidget.close()
