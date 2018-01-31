f2a35d8246ec643f581b49550676d6f4ab4f234f
Thor Johnson <gthormj@gmail.com>
2018-01-30 20:02:28 -0500

    RUNS.  I think I have the load settings working.

--- global_variables.py
+++ global_variables.py
@@ -1,1 +1,12 @@
 _keyboard = None    #instance of Keyboard from kivy.core.window request_keyboard() method
+#BackgroundImage Settings
+_backgroundFile = ""
+_backgroundTLHSV = [(30,40,80),(90,255,255)]
+_backgroundTRHSV = [(160, 60, 40),(10,255,255)]
+_backgroundBLHSV = [(90,60,80),(140,255,255)]
+_backgroundBRHSV = [(160, 60, 40),(10,255,255)]
+_backgroundTRPOS = ( 1225, 615)
+_backgroundTLPOS = (-1225, 615)
+_backgroundBLPOS = (-1225,-615)
+_backgroundBRPOS = ( 1225,-615)
+
