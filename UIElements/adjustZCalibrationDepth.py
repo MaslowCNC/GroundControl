1669ee4a0877c678767d95af24fc4ffd0102eb8e
Barbour Smith <bar@makesmithtech.com>
2018-02-01 13:54:41 -0800

    Switch to using the normal z-axis popup

--- UIElements/adjustZCalibrationDepth.py
+++ UIElements/adjustZCalibrationDepth.py
@@ -1,8 +1,8 @@
 from   kivy.uix.widget                    import   Widget
 from   kivy.properties                    import   ObjectProperty
-from   kivy.properties                    import   StringProperty
 from   UIElements.touchNumberInput        import   TouchNumberInput
 from   kivy.uix.popup                     import   Popup
+from UIElements.zAxisPopupContent              import ZAxisPopupContent
 
 class AdjustZCalibrationDepth(Widget):
     '''
@@ -11,6 +11,7 @@ class AdjustZCalibrationDepth(Widget):
     
     '''
     data                         =  ObjectProperty(None) #linked externally
+    zStepSizeVal                 =  .1                   #The default movement size for the z-axis
     
     def on_enter(self):
         '''
@@ -20,27 +21,11 @@ class AdjustZCalibrationDepth(Widget):
         '''
         if int(self.data.config.get('Maslow Settings', 'zAxis')) == 1:
             self.zAxisActiveSwitch.active = True
+            self.openZPopupBtn.disabled        = False
         else:
             self.zAxisActiveSwitch.active = False
+            self.openZPopupBtn.disabled        = True
     
-    def moveZ(self, dist):
-        '''
-
-        Move the z-axis the specified distance
-
-        '''
-        self.data.units = "MM"
-        self.data.gcode_queue.put("G21 ")
-        self.data.gcode_queue.put("G91 G00 Z" + str(dist) + " G90 ")
-
-    def zeroZ(self):
-        '''
-
-        Define the z-axis to be currently at height 0
-
-        '''
-        self.data.gcode_queue.put("G10 Z0 ")
-        self.carousel.load_next()
     
     def enableZaxis(self, *args):
         '''
@@ -51,3 +36,38 @@ class AdjustZCalibrationDepth(Widget):
         self.data.config.set('Maslow Settings', 'zAxis', int(self.zAxisActiveSwitch.active))
         self.data.config.write()
         self.data.pushSettings()
+        
+        #enable and disable the button to open the z-axis popup
+        if self.zAxisActiveSwitch.active == True:
+            self.openZPopupBtn.disabled        = False
+        else:
+            self.openZPopupBtn.disabled        = True
+    
+    def zAxisPopup(self):
+        self.popupContent      = ZAxisPopupContent(done=self.dismissZAxisPopup)
+        self.popupContent.data = self.data
+        self.popupContent.initialize(self.zStepSizeVal)
+        self._zpopup = Popup(title="Z-Axis", content=self.popupContent,
+                            size_hint=(0.5, 0.5))
+        self._zpopup.open()
+    
+    def dismissZAxisPopup(self):
+        '''
+        
+        Close The Z-Axis Pop-up
+        
+        '''
+        try:
+            self.zStepSizeVal = float(self.popupContent.distBtn.text)
+        except:
+            pass
+        self._zpopup.dismiss()
+    
+    def zeroZ(self):
+        '''
+
+        Define the z-axis to be currently at height 0
+
+        '''
+        self.data.gcode_queue.put("G10 Z0 ")
+        self.carousel.load_next()
