53d1e72c8dd8367a94bb3a0e219f377134465188
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-02-20 11:32:24 -0800

    Create a template widget for how calibration widgets should be

---
+++ CalibrationWidgets/calibrationStepTemplate.py
@@ -0,0 +1,13 @@
+'''
+
+A template for creating a new calibration step widget
+
+'''
+from   kivy.uix.gridlayout							import   GridLayout
+from   kivy.properties								import   ObjectProperty
+
+class CalibrationTemplate(GridLayout):
+    done   = ObjectProperty(None)
+    
+    
+    
