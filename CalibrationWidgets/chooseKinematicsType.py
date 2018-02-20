3bde3910d3db812290da6fb31c258fac35d5fcee
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-02-20 11:59:13 -0800

    Create blank templates for each step

--- CalibrationWidgets/chooseKinematicsType.py
+++ CalibrationWidgets/chooseKinematicsType.py
@@ -6,20 +6,8 @@ A template for creating a new calibration step widget
 from   kivy.uix.gridlayout							import   GridLayout
 from   kivy.properties								import   ObjectProperty
 
-class CalibrationStepTemplate(GridLayout):
+class ChooseKinematicsType(GridLayout):
     done   = ObjectProperty(None)
     
     
-    def onEnter(self):
-		'''
-		
-		This function runs when the step is entered
-		
-		'''
-	
-	def onExit(self):
-		'''
-		
-		This function run when the step is completed
-		
-		'''
+    
