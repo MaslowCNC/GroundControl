d48daeb5de7f1ce2f7b95260eebc28a0d2263e08
BarbourSmith <BarbourSmith@users.noreply.github.com>
2018-03-05 13:42:53 -0800

    tested here

--- CalibrationWidgets/measureOutChains.py
+++ CalibrationWidgets/measureOutChains.py
@@ -21,6 +21,15 @@ class MeasureOutChains(GridLayout):
         '''
         self.data = App.get_running_app().data
         self.text =  "Now we are going to measure out the chains and reattach the sled\n\nHook the first link of the right chain on the vertical tooth of the right sprocket\n as shown in the picture below\n\nThe left chain does not need to be moved, it can be left partly extended\n\nThe correct length of first the left and then the right chain will be measured out\n\nOnce both chains are finished attach the sled, then press Next\nPressing Next will move the sled to the center of the sheet.\n\nBe sure to keep an eye on the chains during this process to ensure that they do not become tangled\naround the sprocket. The motors are very powerful and the machine can damage itself this way"
+        
+        #select the right image for a given setup
+        print "measure out chains on enter"
+        if App.get_running_app().data.config.get('Advanced Settings', 'chainOverSprocket') == 'Top':
+            print "top feeding detected"
+            self.leftImg.source = "./Documentation/Calibrate Machine Dimensions/topfeeding/Ready To Calibrate.jpg"
+        else :
+            print "bottom feeding detected"
+            self.leftImg.source = "./Documentation/Calibrate Machine Dimensions/bottomfeeding/Ready To Calibrate.jpg"
     
     def stop(self):
         self.data.quick_queue.put("!") 
