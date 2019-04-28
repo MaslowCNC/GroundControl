7163148021f620cb43799cca6fb5526ed8c1fe86
schmittjoshc <35613624+schmittjoshc@users.noreply.github.com>
2019-04-28 13:49:37 -0500

    Using firmwareKeyValue from data
    Related to import issue mentioned in prior commit.  Fixing the issue by passing firmwareKeyValue function through data, rather than importing.

--- UIElements/firmwareSyncPopup.py
+++ UIElements/firmwareSyncPopup.py
@@ -8,14 +8,15 @@ Created on Fri Apr 26 22:38:41 2019
 from   kivy.uix.popup                            import   Popup
 
 class FirmwareSyncPopup(Popup):
-    def __init__(self,GcVal,FwVal,Section,Key):
+    def __init__(self,data,GcVal,FwVal,Section,Key):
         super(FirmwareSyncPopup,self).__init__()
+        self.data=data
         self.GcVal=GcVal
         self.FwVal=FwVal
         self.Section=Section
         self.Key=Key
-        self.ids['GcVal'].text=str(GcVal)
-        self.ids['FwVal'].text=str(FwVal)
+        self.ids['GcVal'].text=data.firmwareKeyValue(GcVal)
+        self.ids['FwVal'].text=data.firmwareKeyValue(FwVal)
         self.ids['KeyLabel'].text='{}\n{}'.format(Section,Key)
     def SelectGroundControlValue(self):
         self.data.config.set(self.Section,self.Key,self.GcVal)
