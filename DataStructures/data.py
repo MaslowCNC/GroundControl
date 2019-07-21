cf2d955009a19d8eb426152d48fd8c61362fb7e2
Scott Smith <scottsm@mac.com>
2019-07-20 17:26:51 -0700

    bump version number

--- DataStructures/data.py
+++ DataStructures/data.py
@@ -26,7 +26,7 @@ class Data(EventDispatcher):
     
     #Gcodes contains all of the lines of gcode in the opened file
     gcode      = ObjectProperty([])
-    version    = '1.26'
+    version    = '1.27'
     #all of the available COM ports
     comPorts   = []
     #This defines which COM port is used
